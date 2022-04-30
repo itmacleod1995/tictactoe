a35b56963dda91d7d78fbbb5cacae796c405e6f1
Ian <itmacleod@email.wm.edu>
2022-04-30 14:24:37 -0400

    Got it so that the game recognizes when three in a row have been made

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ print("Whoever gets three in a row is the winner!\n")
 WINNERCELLS = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
 selectedSpots = []
 
+player1Cells = []
+player2Cells = []
+
 board = """
     _7_|_8_|_9_
     _4_|_5_|_6_
@@ -20,7 +23,7 @@ board = """
 """
 
 
-def makeChoice(player, b):
+def makeChoice(player, b, playerNum):
     """Function that allows the player to play their piece on the board"""
     chooseAgain = True
     while chooseAgain:
@@ -29,11 +32,32 @@ def makeChoice(player, b):
             print("That cell has already been picked! Please pick again ")
         else:
             selectedSpots.append(choice)
+            if playerNum == 1:
+                player1Cells.append(choice)
+            else:
+                player2Cells.append(choice)
             updatedboard = b.replace(choice, player)
             print(updatedboard)
             chooseAgain = False
 
         return updatedboard
+
+def checkIfThreeInARow(selectedSpots, winningcells):
+    """Method that checks if the player has gotten three in a row"""
+    count = 0
+    for cell in winningcells:
+        for num in cell:
+            if str(num) not in selectedSpots:
+                count = 0
+                break
+            elif str(num) in selectedSpots:
+                count += 1
+                if count == 3:
+                    #print("Selected Spots = {}".format(selectedSpots))
+                    return True
+    #print("Selected Spots = {}".format(selectedSpots))
+    return False
+
 
 
 game = True
@@ -50,9 +74,15 @@ while game:
 
     if turn % 2 != 0:
         print("Player 1, choose location: ", end="")
-        board = makeChoice(player1, board)
+        board = makeChoice(player1, board, 1)
+        if checkIfThreeInARow(selectedSpots=player1Cells, winningcells=WINNERCELLS):
+            print("Three in a row! Player 1 wins!")
+            game = False
     else:
         print("Player 2, choose location: ", end="")
-        board = makeChoice(player2, board)
+        board = makeChoice(player2, board, 2)
+        if checkIfThreeInARow(selectedSpots=player2Cells, winningcells=WINNERCELLS):
+            print("Three in a row! Player 2 wins!")
+            game = False
 
     turn += 1
