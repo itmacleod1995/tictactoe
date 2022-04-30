183e5e83d8ce526669aed2861b8f7f2ce6cf0fd5
Ian <itmacleod@email.wm.edu>
2022-04-30 13:12:17 -0400

    Typed up rules

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome to the tic tac toe game!\n")
+print("----Rules----")
+print("The game board is set up as follows")
+print("""
+    _7_|_8_|_9_
+    _4_|_5_|_6_
+     1 | 2 | 3
+""")
+
+print("Each player picks a digit on their keyboard corresponding to the cell they want to place")
+print("Whoever gets three in a row is the winner!")
